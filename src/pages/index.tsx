b3e16ec94a724a61a0fd1a07078540b105436fa1
shadowfaz16 <erikfazju@gmail.com>
2023-02-14 19:21:13 -0600

    change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-import { useState } from 'react'
-import axios from 'axios'
-import Navbar from '@/components/Navbar'
-import { ToastContainer, toast } from "react-toastify";
-import Stable from '../components/Stable'
+
 
 
 const inter = Inter({ subsets: ['latin'] })
