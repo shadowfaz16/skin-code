dc725ac11f169da38d12a7ed6687bc81e83623c8
shadowfaz16 <erikfazju@gmail.com>
2023-02-14 19:18:47 -0600

    change

--- src/pages/models/stable-axios.tsx
+++ src/pages/models/stable-axios.tsx
@@ -34,7 +34,7 @@ const StableAxios = (props: Props) => {
        url: URL,
        method: "POST",
        headers: {
-         Authorization: `Bearer ${process.env.REACT_APP_HUGGING_FACE_API_KEY}`,
+         Authorization: process.env.REACT_APP_HUGGING_FACE_API_KEY,
          Accept: "application/json",
          "Content-Type": "application/json",
        },
